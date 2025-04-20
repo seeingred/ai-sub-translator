9fd5f514c8f570022a36aa5b4f8e8f316fa48435
aka <aka@aka.aka>
2025-04-20 16:11:52 +0400

    fixed linux builds

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
@@ -18,8 +16,6 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     icon: './images/icon',
-    // Note: To build for multiple architectures, use CLI flags:
-    // npm run make -- --arch=x64,arm64
   },
   rebuildConfig: {},
   makers: [
@@ -33,12 +29,6 @@ const config: ForgeConfig = {
     ]),
     // Include MakerZIP for all platforms
     new MakerZIP({}, ['darwin', 'win32', 'linux']), 
-    new MakerRpm({}), 
-    new MakerDeb({
-      options: {
-        icon: './images/icon.png'
-      }
-    })
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
